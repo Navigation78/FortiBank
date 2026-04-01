a11f0bae185b0ac9e059ee22e16c4b2a3e18c89f
Migwi Julia <juliakaguremigwi@gmail.com>
2026-04-02 00:05:21 +0300

    Solved the rendering of the login page since it was displaying plain HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,3 @@ module.exports = {
   },
   plugins: [],
 }
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,jsx,ts,tsx,mdx}',
-    './src/components/**/*.{js,jsx,ts,tsx,mdx}',
-    './src/app/**/*.{js,jsx,ts,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}

09ad77e537788a8d4b0200d3ee30a3d824ae71cd
Migwi Julia <juliakaguremigwi@gmail.com>
2026-05-09 15:56:55 +0300

    Upgrading to next.js version 16.Yeyyy!

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.56.1'],
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', '192.168.56.1:3000'],
-    },
-  },
 }
 
 export default nextConfig
